0e38dcebd0033568c2f59c7714847c23c9cfe1d7
ryzecous <r3j9c89@gmail.com>
2020-12-03 14:13:23 -0500

    pseudo code the script

---
+++ script.js
@@ -0,0 +1,16 @@
+// Create a start Button
+
+    // Create a running timer that counts down when agree to take the quiz
+
+    // Ask JavaScript questions while 
+        // For each correct answer add 1 to the score
+        // For each wrong answer subtract time from the countdown timer
+
+    // When timer runs out
+        // Record Score with a prompt to enter initials
+        // Display Scoreboard
+
+
+
+// View Highscores button
+// Restart Quiz Button
