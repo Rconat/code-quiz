5fb7f3b6474d039e9171954ecc33afb01347b182
ryzecous <r3j9c89@gmail.com>
2020-12-03 16:33:26 -0500

    updating script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// Start Button
+// DOM elements
 var startQuiz = document.getElementById('startQuizBtn')
 var countdownTimer = document.getElementById('countdownTimer')
+var restartQuiz = document.getElementById('restartQuizBtn')
 
 
 
@@ -9,14 +10,14 @@ var countdownTimer = document.getElementById('countdownTimer')
 
 
 // Score variables
-var currentScore = 0
-var finalScore = 0
+var currentScore = 0;
+var finalScore = 0;
 
-// Timer starting time
-var count = 75
-
-startQuiz.addEventListener('click', startQuizfunction() {
-    
+startQuiz.addEventListener('click', startQuizFunction())
+// function runs on startQuiz click
+function startQuizFunction() {
+    // Timer starting time
+    var count = 75;
     // Create a running timer that counts down when agree to take the quiz
     setInterval(function() {
         count--
@@ -26,30 +27,27 @@ startQuiz.addEventListener('click', startQuizfunction() {
 
      // Ask JavaScript questions while 
         // For each correct answer add 1 to the score
-        currentScore++
+        // currentScore++
         // For each wrong answer subtract time from the countdown timer
-        count-5
+        // count-5
 
-    // When timer runs out
-        if (count <= 0) {
+        // When timer runs out
+    if (count <= 0) {
             currentScore = finalScore
             // Display final score
             alert("Quiz Complete: Your Score final score is " + finalScore)
-            // Record Score with a prompt to enter initials
-            var hsList = confirm ('Enter score into Highscores list?')
-            if (hsList)
             // Display Scoreboard
-        }
-})
+    }
+}
 
 // View Highscores button
+
 // Restart Quiz Button
-restartQuiz.addEventListener('click', restartQuiz() {
+restartQuiz.addEventListener('click', restartQuizFunction())
+// function runs on restartQuiz click
+function restartQuizFunction() {
     restartCnfr = confirm("Would you like to restart the quiz?")
     if (restartCnfr) {
-        startQuiz()
-    } else {
-        return
+        startQuizFunction()
     }
-
-})
+}
