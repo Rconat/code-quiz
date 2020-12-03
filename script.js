8a13fe45655a8e147bfdb1a5f5cf931d2529d84b
ryzecous <r3j9c89@gmail.com>
2020-12-03 14:36:53 -0500

    added variables and countdown timer

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-// Create a start Button
+// Start Button
+var startQuiz = document.getElementById('startQuizBtn')
+var countdownTimer = document.getElementById('countdownTimer')
 
+// Score variables
+var currentScore
+var finalScore
+
+// Timer starting time
+var count = 75
+    
     // Create a running timer that counts down when agree to take the quiz
+    setInterval(function() {
+        count--
+        countdownTimer.innerText = count + ' seconds left'
+        console.log(count + ' seconds left')
+    }, 10000)
 
     // Ask JavaScript questions while 
         // For each correct answer add 1 to the score
